b0e8b738e0236b370fa04f0bc8d4123a144087f8
espdev <esp.home@gmail.com>
2014-05-12 20:10:34 +0400

    Used settings.local_srv by default.

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.postgres_local")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.local_srv")
     sys.path.append('readthedocs')
 
     from django.core.management import execute_from_command_line
