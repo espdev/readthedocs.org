e37b7d750dcd9414fc08fc7b049df18317ec3bde
espdev <esp.home@gmail.com>
2014-05-12 14:46:22 +0400

    Added config "postgres_local". It's used by default.

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.sqlite")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.postgres_local")
     sys.path.append('readthedocs')
 
     from django.core.management import execute_from_command_line
